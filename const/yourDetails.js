78c3620f35573658d444582548686a3e5a5ce279
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-05-28 21:32:39 +0000

    Install thirdweb nft-gated

---
+++ const/yourDetails.js
@@ -0,0 +1,11 @@
+// Replace this with your contract address
+export const contractAddress = "0x12A243D816aE3B64dC23def3821fe8A9549EF42b";
+
+// Replace this with your domain name.
+export const domainName = "example.com";
+
+// Minimum number of tokens required to access
+export const minimumBalance = 1;
+
+// Token ID for ERC-1155 tokens
+export const erc1155TokenId = 0;
