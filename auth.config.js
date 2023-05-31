380d32a5c408c1a93ea9090334899ff8965f55c9
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-05-31 19:34:01 +0300

    Use the dotenv

--- auth.config.js
+++ auth.config.js
@@ -1,6 +1,9 @@
 import { ThirdwebAuth } from "@thirdweb-dev/auth/next";
 import { PrivateKeyWallet } from "@thirdweb-dev/auth/evm";
 import { domainName } from "./const/yourDetails";
+// import { dotenv } from 'dotenv';
+
+require('dotenv').config();
 
 export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
   domain: domainName,
