8923136840d3b474dc5187093b4ca4336788f41e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-06-01 05:44:52 +0300

    Change the title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }) {
       }}
     >
       <Head>
-        <title>NFT Gated Website</title>
+        <title>S3SH Game | Login</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
