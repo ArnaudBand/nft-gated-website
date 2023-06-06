8bf3a4d5b4d43918b3d061d331d7e99461d17b2e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-06-06 01:09:48 +0000

    Change the minimum balance

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -5,7 +5,7 @@ export const contractAddress = "0x7B92f22A54b89E32fB2395ed5787D7894f789dfd";
 export const domainName = "http://jired.com/";
 
 // Minimum number of tokens required to access
-export const minimumBalance = 1;
+export const minimumBalance = 0;
 
 // Token ID for ERC-1155 tokens
 export const erc1155TokenId = 0;
